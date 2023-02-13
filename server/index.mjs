e8d69e31a420bdbae5a3bd1420aad635941c1c8c
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-02-12 21:33:53 -0500

    added all env variables

--- server/index.mjs
+++ server/index.mjs
@@ -13,25 +13,31 @@ const props = {
   'server-portv6': process.env.SERVER_PORTV6,
   'enable-lan-visibility': process.env.ENABLE_LAN_VISIBILITY,
   'view-distance': process.env.VIEW_DISTANCE,
-  'tick-distance': 4,
-  'player-idle-timeout': 0,
-  'max-threads': 0,
+  'tick-distance': process.env.TICK_DISTANCE,
+  'player-idle-timeout': process.env.PLAYER_IDLE_TIMEOUT,
+  'max-threads': process.env.MAX_THREADS,
   'level-name': 'Bedrock level',
-  'level-seed': '',
-  'default-player-permission-level': 'member',
-  'texturepack-required': 'true',
-  'content-log-file-enabled': 'false',
-  'compression-threshold': 1,
-  'compression-algorithm': 'zlib',
-  'server-authoritative-movement': 'server-auth',
-  'player-movement-score-threshold': 20,
-  'player-movement-action-direction-threshold': 0.85,
-  'player-movement-distance-threshold': 0.3,
-  'player-movement-duration-threshold-in-ms': 500,
-  'correct-player-movement': false,
-  'server-authoritative-block-breaking': false,
-  'chat-restriction': 'None',
-  'disable-player-interaction': false,
+  'level-seed': process.env.LEVEL_SEED,
+  'default-player-permission-level':
+    process.env.DEFAULT_PLAYER_PERMISSION_LEVEL,
+  'texturepack-required': process.env.TEXTUREPACK_REQUIRED,
+  'content-log-file-enabled': process.env.CONTENT_LOG_FILE_ENABLED,
+  'compression-threshold': process.env.COMPRESSION_THRESHOLD,
+  'compression-algorithm': process.env.COMPRESSION_ALGORITHM,
+  'server-authoritative-movement': process.env.SERVER_AUTHORITATIVE_MOVEMENT,
+  'player-movement-score-threshold':
+    process.env.PLAYER_MOVEMENT_SCORE_THRESHOLD,
+  'player-movement-action-direction-threshold':
+    process.env.PLAYER_MOVEMENT_ACTION_DIRECTION_THRESHOLD,
+  'player-movement-distance-threshold':
+    process.env.PLAYER_MOVEMENT_DISTANCE_THRESHOLD,
+  'player-movement-duration-threshold-in-ms':
+    process.env.PLAYER_MOVEMENT_DURATION_THRESHOLD_IN_MS,
+  'correct-player-movement': process.env.CORRECT_PLAYER_MOVEMENT,
+  'server-authoritative-block-breaking':
+    process.env.SERVER_AUTHORITATIVE_BLOCK_BREAKING,
+  'chat-restriction': process.env.CHAT_RESTRICTION,
+  'disable-player-interaction': process.env.DISABLE_PLAYER_INTERACTION,
 };
 
 (() => {
@@ -41,5 +47,5 @@ const props = {
     str += `${key}=${value}\n`;
   });
 
-  fs.writeFileSync('./bedrock-server-server.properties', str);
+  fs.writeFileSync('./bedrock-server/server.properties', str);
 })();
